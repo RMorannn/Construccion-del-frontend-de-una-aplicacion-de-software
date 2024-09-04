331ef9ad6d40f250b052649d336e261ef4f3bea3
RMorannn <robertomoran9@hotmail.com>
2024-09-03 21:50:02 -0500

    Add files via upload

---
+++ gestion-educativa/backend/app/database.py
@@ -0,0 +1,9 @@
+# backend/app/database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
